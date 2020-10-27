b0cd44b9c01b0e941854956afb35b09740e0c911
Roman <romandub25@gmail.com>
2020-10-27 11:30:08 +0400

    table & clock & navbar

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
