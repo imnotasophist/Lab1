6a0b2e1488b600216c1185b58b77b03731680acd
Roman <romandub25@gmail.com>
2020-11-18 20:50:01 +0300

    db

--- books.php
+++ books.php
@@ -3,105 +3,71 @@ $title = 'Книги';
 require_once '/tpl/header.php'; 
 ?>
     <!-- *Main -->
-<?php 
-$category = '<li class="aside__item main__text"><a class="aside__link" href="#">Художественная литература</a></li>
-            <li class="aside__item main__text"><a class="aside__link" href="#">Книги для детей</a></li>
-            <li class="aside__item main__text"><a class="aside__link" href="#">Образование</a></li>
-            <li class="aside__item main__text"><a class="aside__link" href="#">Наука и техника</a></li>
-            <li class="aside__item main__text"><a class="aside__link" href="#">Общество</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Деловая литература</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Красота. Здоровье. Спорт</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Увлечения</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Психология</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Эзотерика</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Философия и религия</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Искусство</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Подарочные издания</a></li>
-            <li class="aside__item main__text"><a href="#" class="aside__link">Книги на иностранных языках</a></li>';   
-$cards = '<div class="card-deck">
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/first.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Собачье сердце</h5>
-            <p class="card-text">Булгаков М.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">83 ₽</p>
+    <main class="main">
+        <div class="container">
+            <div class="row">
+                <aside class="aside col-xl-2">
+                    <p class="main__text main__text_bold">Категории</p>
+                    <ul>
+                        <li class="aside__item main__text"><a class="aside__link" href="#">Художественная литература</a></li>
+                        <li class="aside__item main__text"><a class="aside__link" href="#">Книги для детей</a></li>
+                        <li class="aside__item main__text"><a class="aside__link" href="#">Образование</a></li>
+                        <li class="aside__item main__text"><a class="aside__link" href="#">Наука и техника</a></li>
+                        <li class="aside__item main__text"><a class="aside__link" href="#">Общество</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Деловая литература</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Красота. Здоровье. Спорт</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Увлечения</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Психология</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Эзотерика</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Философия и религия</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Искусство</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Подарочные издания</a></li>
+                        <li class="aside__item main__text"><a href="#" class="aside__link">Книги на иностранных языках</a></li>
+                    </ul>
+                </aside>
+                <section class="section col-xl-10">
+                    <h2 class="main__title">Книги</h2>
+                    <div class="card-deck">
+                        <?php 
+                        $cards = getCardsAll(); 
+                        foreach ($cards as $card): ?>
+                        <div class="card">
+                            <div class="wrapper">
+                                <img src="<?php echo $card["img"]; ?>" class="card-img-top card__img" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $card["title"]; ?></h5>
+                                <p class="card-text"><?php echo $card["author"]; ?></p>
+                                <div class="card__footer">
+                                    <button class="btn btn-primary main__btn btn__text">Купить</button>
+                                    <p class="main__text main__text_bold"><?php echo $card["price"]; ?> ₽</p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                    </div> 
+                    <div class="card-deck">
+                        <?php 
+                        $cards2 = getCardsAll2(); 
+                        foreach ($cards2 as $card2): ?>
+                        <div class="card">
+                            <div class="wrapper">
+                                <img src="<?php echo $card2["img"]; ?>" class="card-img-top card__img" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $card2["title"]; ?></h5>
+                                <p class="card-text"><?php echo $card2["author"]; ?></p>
+                                <div class="card__footer">
+                                    <button class="btn btn-primary main__btn btn__text">Купить</button>
+                                    <p class="main__text main__text_bold"><?php echo $card2["price"]; ?> ₽</p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                </section>
             </div>
         </div>
-    </div>
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/second.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Портрет Дориана Грея</h5>
-            <p class="card-text">Уайльд О.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">92 ₽</p>
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/third.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Фауст</h5>
-            <p class="card-text">Гете И.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">117 ₽</p>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card-deck">
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/fourth.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Гранатовый браслет</h5>
-            <p class="card-text">Куприн А.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">137 ₽</p>
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/fivth.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Замок</h5>
-            <p class="card-text">Кафка Ф.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">100 ₽</p>
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="wrapper">
-            <img src="img/books/sixth.jpg" class="card-img-top card__img" alt="...">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Преступление и наказание</h5>
-            <p class="card-text">Достоевский Ф.</p>
-            <div class="card__footer">
-                <button class="btn btn-primary main__btn btn__text">Купить</button>
-                <p class="main__text main__text_bold">117 ₽</p>
-            </div>
-        </div>
-    </div>
-</div>';
-$subtitle = $title;
-require '/tpl/main.php'; 
-?>
+    </main>
     <!-- *Footer -->
 <?php include_once '/tpl/footer.php'; ?>
---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+$dbhost   = "localhost";
+$dbname   = "lab4";
+$username = "root";
+$password = "";
+
+$db = new PDO("mysql:host=$dbhost; dbname=$dbname", $username, $password);
+
+function getCardsAll() {
+    global $db;
+    $cards = $db->query("SELECT * FROM cards");
+    return $cards;
+}
+function getCardsAll2() {
+    global $db;
+    $cards2 = $db->query("SELECT * FROM cards2");
+    return $cards2;
+}
