9594d730a944437a21b78d2eec4a86d30a657f23
Roman <romandub25@gmail.com>
2020-11-19 16:15:11 +0400

    autorization & registration

--- tpl/header.php
+++ tpl/header.php
@@ -42,6 +42,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="about.php">Размер</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/autorization.php">Войти</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
